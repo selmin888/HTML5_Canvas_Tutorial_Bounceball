80adae1dc6722e1488a09d4d12a0fad489819196
selmin888 <68253712+selmin888@users.noreply.github.com>
2022-02-07 16:24:57 +0900

    ball

---
+++ app.js
@@ -0,0 +1,30 @@
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+        document.body.appendChild(this.canvas);
+        
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+    
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+/* 공이 브라우저 벽면에 튕기는 애니메이션을 할 경우 canvas.width가 아니라 stageWidth에 튕기도록 해줘야 한다.
+canvas.width에 튕기게 하면 공이 밖으로 삐져나가버린다
+캔버스의 width와 height를 가각 두 배 해주면 캔버스를 브라우저 창에 다 담지 못하게 된다
+스크롤하면 볼 수 있다
+따라서 캔버스를 브라우저 내에 담아내도록 해야한다*/
+        this.canvas.width = this.stageWidth * 2;
+        this.canvas.height = this.stageHeight * 2;
+        this.ctx.scale(2,2); /*하지만 ctx로 그린 공의 크기가 너무 작아졌다. 
+        캔버스의 크기가 커진만큼 ctx의 크기도 키워줘야 처음에 의도했던 비율대로 그림이 그려지게 된다*/ 
+    }
+}
+window.onload = () => {
+    new App();
+}
